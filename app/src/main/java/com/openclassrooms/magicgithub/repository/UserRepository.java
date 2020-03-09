946bcbb1aaf860b69228b21705f0e8e919ce3617
T0N10T0 <supertonioto@gmail.com>
2020-03-09 16:10:22 +0100

    Projet #2

---
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -0,0 +1,27 @@
+package com.openclassrooms.magicgithub.repository;
+
+import com.openclassrooms.magicgithub.api.ApiService;
+import com.openclassrooms.magicgithub.model.User;
+
+import java.util.List;
+
+public class UserRepository {
+
+    private final ApiService apiService;
+
+    public UserRepository(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    public List<User> getUsers() {
+        return this.apiService.getUsers();
+    }
+
+    public void generateRandomUser() {
+        this.apiService.generateRandomUser();
+    }
+
+    public void deleteUser(User user) {
+        this.apiService.deleteUser(user);
+    }
+}
